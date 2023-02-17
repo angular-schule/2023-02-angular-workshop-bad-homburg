a7aadcbb7070c7f634764aa4fa6fa2a54a4c1142
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-02-17 16:30:19 +0100

    ngrx: selectors

--- book-rating/src/app/books/dashboard/dashboard.component.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.ts
@@ -1,7 +1,9 @@
 import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { Book } from '../shared/book';
 import { BookRatingService } from '../shared/book-rating.service';
 import { BooksService } from '../shared/http';
+import { selectBooks, selectLoading } from '../store/book.selectors';
 
 @Component({
   selector: 'br-dashboard',
@@ -11,46 +13,51 @@ import { BooksService } from '../shared/http';
 })
 export class DashboardComponent {
 
-  br = inject(BookRatingService);
-  bs = inject(BooksService);
+
+  books$ = inject(Store).select(selectBooks);
+  loading$ = inject(Store).select(selectLoading);
+
+
+  // br = inject(BookRatingService);
+  // bs = inject(BooksService);
 
   constructor() {
-    this.bs.booksGet().subscribe(books => this.books = books)
+    // this.bs.booksGet().subscribe(books => this.books = books)
   }
 
   selectedBook?: Book;
-  books: Book[] = [];
+  // books: Book[] = [];
 
   doRateUp(book: Book): void {
-    const ratedBook = this.br.rateUp(book);
-    // const ratedBook = {
-    //   ...book,
-    //   rating: book.rating < 5 ? book.rating + 1 : 5
-    // };
-    this.updateAndSort(ratedBook);
+    // const ratedBook = this.br.rateUp(book);
+    // // const ratedBook = {
+    // //   ...book,
+    // //   rating: book.rating < 5 ? book.rating + 1 : 5
+    // // };
+    // this.updateAndSort(ratedBook);
   }
 
   doRateDown(book: Book): void {
-    const ratedBook = this.br.rateDown(book);
-    this.updateAndSort(ratedBook);
+    // const ratedBook = this.br.rateDown(book);
+    // this.updateAndSort(ratedBook);
   }
 
   updateAndSort(ratedBook: Book) {
-    this.books = this.books
-      .map(b => b.isbn === ratedBook.isbn ? ratedBook : b)
-      .sort((a, b) => b.rating - a.rating);
+    // this.books = this.books
+    //   .map(b => b.isbn === ratedBook.isbn ? ratedBook : b)
+    //   .sort((a, b) => b.rating - a.rating);
   }
 
   addBook(newBook: Book) {
-    this.books = [...this.books, newBook];
+    // this.books = [...this.books, newBook];
   }
 
   changeToEditMode(book: Book) {
-    this.selectedBook = book;
+    // this.selectedBook = book;
   }
 
   changeBook(changedBook: Book) {
-    this.updateAndSort(changedBook);
-    this.selectedBook = undefined;
+    // this.updateAndSort(changedBook);
+    // this.selectedBook = undefined;
   }
 }
