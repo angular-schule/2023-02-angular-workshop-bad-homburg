d7d6fc664832e47130b74b24f4e4cf8247c07577
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-02-17 13:55:54 +0100

    rxjs: one-time error handling

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,6 +1,7 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { map, switchMap } from 'rxjs';
+import { catchError, map, of, switchMap } from 'rxjs';
 
 import { BooksService } from '../shared/http';
 
@@ -17,7 +18,13 @@ export class BookDetailsComponent {
 
   book$ = inject(ActivatedRoute).paramMap.pipe(
     map(paramMap => paramMap.get('isbn') ?? ''),
-    switchMap(isbn => this.bs.booksIsbnGet(isbn))
+    switchMap(isbn => this.bs.booksIsbnGet(isbn)),
+    catchError((err: HttpErrorResponse) => of({
+      isbn: '000',
+      title: 'FEHLER',
+      description: err.message,
+      rating: 1
+    }))
   )
 
 }
